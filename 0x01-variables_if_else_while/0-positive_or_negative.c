e0428b5e407cbccdb4b2001a5ba60e2cca254b01
binibos <biniyok49@gmail.com>
2022-07-05 02:31:21 +0300

    p or n
    if the number postive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/* 
+* assign a random number to the variable n each time it is executed and
+* print out based on the condition
+ *return zero
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+    if(n>0){
+        printf("%d is positive \n ", n);
+    }
+    if(n==0){
+        printf("%d is zero \n", n);
+    }
+    if(n<0){
+        printf("%d is negative \n",n);
+    }
+	return (0);
+}
